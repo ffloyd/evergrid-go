d694f6edae2d52afd35972f0a048ae6d30b5097e
Roman Kolesnev <rvkolesnev@gmail.com>
2016-05-13 20:44:11 +0300

    Fix GflopsPerMb field behavior

--- simulation/simdata/workloadcfg/datacfg/processorcfg.go
+++ simulation/simdata/workloadcfg/datacfg/processorcfg.go
@@ -3,7 +3,7 @@ package datacfg
 // ProcessorCfgYAML is a representation of processor segment in data yaml file
 type ProcessorCfgYAML struct {
 	Name        string
-	GflopsPerMb float64
+	GflopsPerMb float64 `yaml:"gflops_per_mb"`
 }
 
 // ProcessorCfg is a representation of processor form data config
