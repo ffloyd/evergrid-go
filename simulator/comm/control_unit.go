58f3e7a016c002584e1b75bd98e6cec8355848f2
Roman Kolesnev <rvkolesnev@gmail.com>
2016-06-13 23:52:12 +0300

    Package comm documentation

--- simulator/comm/control_unit.go
+++ simulator/comm/control_unit.go
@@ -2,12 +2,14 @@ package comm
 
 import "github.com/ffloyd/evergrid-go/global/types"
 
-// ControlUnitUploadDataset -
+// ControlUnitUploadDataset - запрос на загрузку датасета
+// Может приходить Control Unit'у либо от Core, либо от другого Control Unit'а (в случае делегирования лидеру)
 type ControlUnitUploadDataset struct {
 	Dataset types.DatasetInfo
 }
 
-// ControlUnitRunExperiment -
+// ControlUnitRunExperiment - запрос на запуск вычислителя с указанным датасетом
+// Может приходить Control Unit'у либо от Core, либо от другого Control Unit'а (в случае делегирования лидеру)
 type ControlUnitRunExperiment struct {
 	Calculator types.CalculatorInfo
 	Dataset    types.DatasetInfo
--- simulator/comm/worker.go
+++ simulator/comm/worker.go
@@ -2,24 +2,26 @@ package comm
 
 import "github.com/ffloyd/evergrid-go/global/types"
 
-// WorkerUploadDataset -
+// WorkerUploadDataset - указание ControlUnit'а Worker'у загрузить датасет
 type WorkerUploadDataset struct {
 	Dataset types.DatasetInfo
 }
 
-// WorkerBuildCalculator -
+// WorkerBuildCalculator - указание ControlUnit'а Worker'у собрать вычислитель
 type WorkerBuildCalculator struct {
 	Calculator types.CalculatorInfo
 }
 
-// WorkerRunCalculator -
+// WorkerRunCalculator  - указание ControlUnit'а Worker'у запустить вычислитель с указанным датасетом
 type WorkerRunCalculator struct {
 	Calculator string
 	Dataset    string
 }
 
-// WorkerBusy -
+// WorkerBusy - запрос ControlUnit'а Worker'у. Если в данный момент выполняется какая-либо задача будет возращено true
 type WorkerBusy struct{}
 
-// WorkerInfo -
+// WorkerInfo - запрос ControlUnit'а Worker'у. Возвращается частично заполненный types.WorkerInfo.
+//
+// Частичная заполненность обуславливается, например, тем, что Worker не знает о том, сколько задачи у него в очереди на выполнение.
 type WorkerInfo struct{}
